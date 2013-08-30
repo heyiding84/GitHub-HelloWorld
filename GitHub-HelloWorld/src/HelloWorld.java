61edb2529e029c3331c5cde0cbb35f8f22fa9481
= <=>
2013-08-30 14:24:44 +0800

    This is the 4th commit. I use Eclipse update GitHub-HelloWorld project.

--- GitHub-HelloWorld/src/HelloWorld.java
+++ GitHub-HelloWorld/src/HelloWorld.java
@@ -4,6 +4,12 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GitHub-HelloWorld!");
+		
+		AnotherClass Second = new AnotherClass();
+		Second.AnotherClass();
+		
+		ThirdClass Third = new ThirdClass();
+		Third.ThirdClass();
 	}
 
 }
